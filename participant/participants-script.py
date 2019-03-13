1c77b5ca9efe8c410d85a8428c65a866ebef1d1f
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-03-13 17:44:01 +0800

    Final state

---
+++ participant/participants-script.py
@@ -0,0 +1,47 @@
+names = []
+speakers = []
+organisations = []
+
+import csv
+
+with open('participants.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        speakers.append(row[0])
+        speakers.append(row[0])
+print(speakers,organisations)
+sIndex = 0
+oIndex = 0
+even = False
+newSVG = []
+for i in range(0,100):
+    with open ("participants.svg", "r") as myfile:
+        data=myfile.readlines()
+        currentSVG = []
+
+    try:
+        for line in data:
+            if 'Name' in line:
+                line = line.replace('Name', speakers[sIndex])
+                print(line)
+                sIndex = sIndex+1
+            if 'Organization' in line:
+                line = line.replace('Organization', organisations[oIndex])
+                print(line)
+                oIndex = oIndex + 1
+
+            currentSVG.append(line)
+
+        newSVG.append(currentSVG)
+
+        with open("output" + str(i) + ".svg", "w") as output:
+            for line in newSVG[i]:
+                output.write(line)
+    except:
+        print('error')
+
+
+# print(newSVG)
+
+
---
+++ participant/svg2pdf.sh
@@ -0,0 +1,6 @@
+for i in {0..84}
+do
+   inkscape output$i.svg --export-text-to-path --export-pdf=output$i.pdf
+
+done
+
